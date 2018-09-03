4a3176e7bbf21a9b71b3cd5b4e366fd01e585340
Pantheon <pantheonshell@yahoo.com>
2018-09-03 19:33:22 -0300

    Done!

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const express = require("express");
+const express_handlebars = require("express-handlebars");
 const app = express();
+
+// Usar Handlebars para arquivos .html
+app.engine("html", express_handlebars({ defaultLayout: "main", extname: "html" }));
+app.set("view engine", "html");
+
+// entregar arquivos estáticos que estão no diretório "static"
+// em /static/*.*
+app.use("/static/", express.static("./static/"));
+
+// Mostrar página inicial
+app.get("/", (req, res) => {
+    res.render("root", { layout: "main" });
+});
 
 app.listen(process.env.PORT || 3000);
